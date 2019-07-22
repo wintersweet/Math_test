0087d32751c2e4179106299c7f3183ee41865adf
wintersweet <wintergrass@126.com>
2019-07-22 15:51:58 +0800

    Initial Commit

---
+++ MathAlgorithm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MathAlgorithm
+//
+//  Created by Leo on 2019/7/22.
+//  Copyright © 2019 dongdonghu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MathAlgorithm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MathAlgorithm
+//
+//  Created by Leo on 2019/7/22.
+//  Copyright © 2019 dongdonghu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
